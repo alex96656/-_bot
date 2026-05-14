5a9c34798ee31ea0f5f15ed030c8a942cd0976cd
〖ᴹᴿ•ᴀʟᴇ᥊᭄𓋆 ⁰⁰³ <godwinuzochi51@gmail.com>
2026-05-14 03:22:10 +0100

    Update whatsapp_telegram_bot_Version2.py

--- whatsapp_telegram_bot_Version2.py
+++ whatsapp_telegram_bot_Version2.py
@@ -4,11 +4,19 @@ import qrcode
 import time
 from io import BytesIO
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
+)
 from telegram import Update, BotCommand
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, ConversationHandler
 import asyncio
