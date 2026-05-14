10c38329659117646b5f268f1662f15e70a7e0bd
〖ᴹᴿ•ᴀʟᴇ᥊᭄𓋆 ⁰⁰³ <godwinuzochi51@gmail.com>
2026-05-14 03:02:23 +0100

    Update whatsapp_telegram_bot_Version2.py

--- whatsapp_telegram_bot_Version2.py
+++ whatsapp_telegram_bot_Version2.py
@@ -386,5 +386,15 @@ def main():
     # Start the Bot
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
-if __name__ == '__main__':
-    main()
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
